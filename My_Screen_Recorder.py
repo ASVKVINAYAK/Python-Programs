02b386543ef721efa58bd9826144c9ae9453bb04
A S V K Vinayak <asvkvinayak@gmail.com>
2021-06-08 12:26:23 +0530

    My Screen Recorder with webcam enabled

--- My_Screen_Recorder.py
+++ My_Screen_Recorder.py
@@ -1,11 +1,26 @@
 from PIL import ImageGrab
 import numpy as np
 import cv2
-from win32api import GetSystemMatrics
+from win32api import GetSystemMetrics
 
+width  = GetSystemMetrics(0)
+height = GetSystemMetrics(1)
+# Give A Uniquename for your file
+name = "Testing"
+file_name = f'{name}.mp4'
+webcam = cv2.VideoCapture(0)
+
+fourcc = cv2.VideoWriter_fourcc('m','p','4','v')
+captured_video = cv2.VideoWriter(file_name,fourcc, 20.0, (width,height))
 while True:
-    img = ImageGrab.grab(bbox=(0,0,1280,720))
+    img = ImageGrab.grab(bbox=(0,0,width,height))
     img_np = np.array(img)
     img_final = cv2.cvtColor(img_np, cv2.COLOR_BGR2RGB)
+    _, frame = webcam.read()
+    fr_height , fr_width, _ = frame.shape
+    img_final[0:fr_height, 0:fr_width :] = frame[0:fr_height, 0:fr_width :] 
     cv2.imshow('Screen Recorder',img_final)
-    cv2.waitKey(10)
+    cv2.imshow('webcam',frame)
+    captured_video.write(img_final)
+    if cv2.waitKey(10) == ord('q'):
+        break 
